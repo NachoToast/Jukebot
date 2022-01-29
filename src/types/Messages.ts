17ef28a82f9a4d589fae06d332d8d4275b69ffdc
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-01-30 01:17:25 +1300

    Make enum members PascalCase

--- src/types/Messages.ts
+++ src/types/Messages.ts
@@ -1,6 +1,6 @@
 enum Messages {
-    guildOnly = 'Sorry, you can only use this command in a server',
-    notInVoice = 'You need to be in a voice channel to use this command',
+    GuildOnly = 'Sorry, you can only use this command in a server',
+    NotInVoice = 'You need to be in a voice channel to use this command',
 }
 
 export default Messages;
