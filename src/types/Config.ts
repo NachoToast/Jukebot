889212a895789a951605034ad96d3790809ad5a6
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-01-29 16:48:31 +1300

    update config documentation

--- src/types/Config.ts
+++ src/types/Config.ts
@@ -2,7 +2,8 @@ export default interface Config {
     /** Link to to the bot's source code. */
     sourceCode: string;
 
-    /** If the bot doesn't connect to Discord after this many seconds, the process will terminate.
+    /** If the bot doesn't connect to Discord after this many seconds,
+     * the process will terminate.
      *
      * Set to 0 to never terminate.
      */
