17ecb1270811a5029ecfe628f60b5f9628250486
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-01-30 23:13:54 +1300

    ideal channel getter helper function

---
+++ src/helpers/getIdealChannel.ts
@@ -0,0 +1,49 @@
+import { Guild, TextChannel } from 'discord.js';
+
+const hasJukebot = new RegExp(/juke(-)?bot/gi);
+const hasCommands = new RegExp(/commands?/gi);
+const hasMusic = new RegExp(/music/gi);
+
+/** Gets the "best" channel to send a message in from a guild.
+ *
+ */
+export async function getIdealChannel(guild: Guild): Promise<TextChannel | null> {
+    const allChannels = await guild.channels.fetch();
+
+    let idealChannel: TextChannel | null = null;
+    let weight = -1;
+
+    for (const [, channel] of allChannels) {
+        if (channel.type !== 'GUILD_TEXT') continue;
+        if (!guild.me) continue;
+        const channelPerms = channel.permissionsFor(guild.me, true);
+        if (!channelPerms.has('SEND_MESSAGES', true)) {
+            continue;
+        }
+        const name = channel.name;
+
+        if (hasJukebot.test(name)) {
+            idealChannel = channel;
+            break;
+        }
+
+        let localWeight = 0;
+        if (hasMusic.test(name)) localWeight = 3;
+        else {
+            // for some reason a regex like `/bot/gi` doesn't work :/
+            const bot = name.toLowerCase().includes('bot');
+
+            const commands = hasCommands.test(name);
+            if (bot && commands) localWeight = 2;
+            else if (commands) localWeight = 1;
+            else if (bot) localWeight = 1;
+        }
+
+        if (localWeight > weight) {
+            idealChannel = channel;
+            weight = localWeight;
+        }
+    }
+
+    return idealChannel;
+}
