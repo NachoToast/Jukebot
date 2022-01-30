472a3f75aa4c8796d678f17d4c1e34eb5a6f6318
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-01-30 14:10:04 +1300

    small music disc tweaks

--- src/classes/MusicDisc.ts
+++ src/classes/MusicDisc.ts
@@ -1,11 +1,9 @@
 import { GuildMember } from 'discord.js';
+import { YouTubeVideo } from 'play-dl';
 import { GuildedInteraction } from '../types/Interactions';
-import { defaultDiscArray, DiscImages, InputDiscData } from '../types/MusicDisc';
-import { stringToNumerical } from '../helpers/timeConverters';
+import { defaultDiscArray, DiscImages } from '../types/MusicDisc';
 
-/** MusicDisc instances represent stored data for a specific song,
- * such as title, duration, and who requested it.
- */
+/** A MusicDisc represents stored metadata for a specific song. */
 export class MusicDisc {
     public readonly addedBy: GuildMember;
     public readonly addedAt: number;
@@ -13,28 +11,32 @@ export class MusicDisc {
     public readonly url: string;
     public readonly title: string;
     public readonly thumbnail: string;
+    public readonly likes: number;
 
     /** The duration of this song in seconds. */
-    public readonly duration: number;
+    public readonly durationSeconds: number;
 
     /** The duration of this song in string form,
      * @example '0:12', '3:45', '5:06:07'.
      */
     public readonly durationString: string;
 
-    public constructor(interaction: GuildedInteraction, data: InputDiscData) {
+    public constructor(interaction: GuildedInteraction, video: YouTubeVideo, title: string) {
+        const { url, durationRaw, durationInSec, thumbnails, likes } = video;
+
         this.addedBy = interaction.member;
         this.addedAt = Date.now();
 
-        this.title = data.title;
-        this.url = data.url;
-        this.durationString = data.duration || '??:??';
-        this.thumbnail = data.thumbnail || MusicDisc.chooseRandomDisc();
+        this.title = title;
+        this.url = url;
+        this.durationString = durationRaw;
+        this.thumbnail = thumbnails.shift()?.url || MusicDisc.chooseRandomDisc();
+        this.likes = likes;
 
-        this.duration = data.duration ? stringToNumerical(data.duration) : 0;
+        this.durationSeconds = durationInSec;
     }
 
-    /** If we can't find a thumbnail URL, this gets a random music disc image. */
+    /** If there isn't thumbnail URL, this gets a random music disc image. */
     private static chooseRandomDisc(): DiscImages {
         const randomIndex = Math.floor(Math.random() * defaultDiscArray.length);
         return defaultDiscArray[randomIndex];
