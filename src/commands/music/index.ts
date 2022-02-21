52fcbb3e9db61a21f4d7903c4ccd6bb236e52feb
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-02-21 16:45:30 +1300

    Load new commands to client

--- src/commands/music/index.ts
+++ src/commands/music/index.ts
@@ -1,6 +1,8 @@
 // import musicTest from './musicTest';
+import clear from './clear';
+import fix from './fix';
 import nowPlaying from './nowPlaying';
 import play from './play';
 import queue from './queue';
 import skip from './skip';
-export default [play, queue, nowPlaying, skip];
+export default [play, queue, nowPlaying, skip, clear, fix];
