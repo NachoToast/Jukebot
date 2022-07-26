91dfd4ec6848a62efcf26ab8e199be1fc567c162
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-07-26 20:49:05 +1200

    remove unnecessary message globals

--- src/global/Messages.ts
+++
@@ -1,5 +0,0 @@
-export const Messages = {
-    cannotJoinThisChannel: (channelId: string) => `I don't have permission to join <#${channelId}>`,
-    notInVoice: () => `You must be in a voice channel to use this command`,
-    timeout: (seconds: number, subject: string) => `Unable to ${subject} in reasonable time (${seconds} seconds)`,
-};
