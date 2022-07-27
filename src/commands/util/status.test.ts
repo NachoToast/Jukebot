a95cdf40f7cf7a8b282564a7f83418e1ffab4ac1
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-07-27 17:28:44 +1200

    fix status tests

--- src/commands/util/status.test.ts
+++ src/commands/util/status.test.ts
@@ -1,5 +1,6 @@
 import { InteractionReplyOptions } from 'discord.js';
 import { Jukebot } from '../../classes/Jukebot';
+import { StatusTiers } from '../../classes/Jukebox/types';
 import { JukebotInteraction } from '../../types/JukebotInteraction';
 import { Status } from './status';
 
@@ -8,7 +9,17 @@ describe(`/status`, () => {
 
     const reply = (newOutput: InteractionReplyOptions) => (output = newOutput);
 
-    const jukebot = { client: { ws: { ping: 123 } }, getJukebox: () => undefined } as unknown as Jukebot;
+    const jukebot = {
+        client: { ws: { ping: 123 }, guilds: { cache: { size: 24 } } },
+        getJukebox: () => undefined,
+        getNumJukeboxes: () => {
+            return {
+                [StatusTiers.Active]: 1,
+                [StatusTiers.Idle]: 2,
+                [StatusTiers.Inactive]: 3,
+            };
+        },
+    } as unknown as Jukebot;
     const interaction = { createdTimestamp: Date.now() - 1000, reply } as unknown as JukebotInteraction;
 
     const params = { jukebot, interaction };
@@ -20,13 +31,21 @@ describe(`/status`, () => {
         expect(output?.content?.includes(`123`));
     });
 
-    it(`should give API latency fairly accurately`, async () => {
-        await status.execute(params);
+    it(`should give API latency fairly accurately`, () => {
         expect(output?.content?.match(/10[0-9]{2}/g));
     });
 
-    it(`should include package version`, async () => {
-        await status.execute(params);
+    it(`should include package version`, () => {
         expect(output?.content?.includes(process.env.npm_version ?? `Unknown`));
+    });
+
+    it(`should show different statuses of all Jukeboxes`, () => {
+        expect(output?.content?.includes(`1 Active`));
+        expect(output?.content?.includes(`2 Idle`));
+        expect(output?.content?.includes(`3 Inactive`));
+    });
+
+    it(`should show number of guilds Jukebot is in`, () => {
+        expect(output?.content?.includes(`24`));
     });
 });
