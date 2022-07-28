74ce75553a9bce51737416d01843fff1299ee8de
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-07-28 17:45:01 +1200

    fix jsdoc comments for getQueueLength

--- src/functions/getQueueLength.ts
+++ src/functions/getQueueLength.ts
@@ -1,10 +1,6 @@
 import { MusicDisc } from '../classes/MusicDisc';
 
-/**
- * Gets the cumulative duration (in seconds) of a queue, excluding live videos.
- *
- * Will return `undefined` if the queue has live videos
- */
+/** Gets the cumulative duration (in seconds) of a queue, excluding live videos. */
 export function getQueueLength(queue: MusicDisc[]): { totalDuration: number; hasLiveVideos: boolean } {
     let totalDuration = 0;
     let hasLiveVideos = false;
