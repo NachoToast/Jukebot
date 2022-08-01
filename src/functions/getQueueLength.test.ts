2041aed1be37b676f8c3ae5be20ec67be2efa9a3
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-08-01 19:25:42 +1200

    more queue length info

--- src/functions/getQueueLength.test.ts
+++ src/functions/getQueueLength.test.ts
@@ -9,7 +9,10 @@ describe(`getQueueLength`, () => {
             { live: false, durationSeconds: 56 },
         ] as MusicDisc[];
 
-        expect(getQueueLength(noLive)).toEqual({ totalDuration: 12 + 34 + 56, numLiveVideos: 0 });
+        const res = getQueueLength(noLive);
+        expect(res.totalDuration).toEqual(12 + 34 + 56);
+        expect(res.numLiveVideos).toEqual(0);
+        expect(res.durationTillFirstLive).toEqual(12 + 34 + 56);
     });
 
     it(`Handles only live queues`, () => {
@@ -19,21 +22,44 @@ describe(`getQueueLength`, () => {
             { live: true, durationSeconds: Number.POSITIVE_INFINITY },
         ] as MusicDisc[];
 
-        expect(getQueueLength(allLive)).toEqual({ totalDuration: 0, numLiveVideos: 3 });
+        const res = getQueueLength(allLive);
+
+        expect(res.totalDuration).toEqual(0);
+        expect(res.numLiveVideos).toEqual(3);
+        expect(res.durationTillFirstLive).toEqual(0);
     });
 
     it(`Handles hybrid queues`, () => {
-        const someLive = [
+        const someLive1 = [
             { live: true, durationSeconds: Number.POSITIVE_INFINITY },
             { live: false, durationSeconds: 12 },
             { live: true, durationSeconds: Number.POSITIVE_INFINITY },
             { live: false, durationSeconds: 34 },
         ] as MusicDisc[];
 
-        expect(getQueueLength(someLive)).toEqual({ totalDuration: 12 + 34, numLiveVideos: 2 });
+        const res1 = getQueueLength(someLive1);
+
+        expect(res1.totalDuration).toEqual(12 + 34);
+        expect(res1.numLiveVideos).toEqual(2);
+        expect(res1.durationTillFirstLive).toEqual(0);
+
+        const someLive2 = [
+            { live: false, durationSeconds: 12 },
+            { live: false, durationSeconds: 34 },
+            { live: true, durationSeconds: Number.POSITIVE_INFINITY },
+            { live: false, durationSeconds: 56 },
+            { live: true, durationSeconds: Number.POSITIVE_INFINITY },
+            { live: false, durationSeconds: 78 },
+        ] as MusicDisc[];
+
+        const res2 = getQueueLength(someLive2);
+
+        expect(res2.totalDuration).toEqual(12 + 34 + 56 + 78);
+        expect(res2.numLiveVideos).toEqual(2);
+        expect(res2.durationTillFirstLive).toEqual(12 + 34);
     });
 
     it(`Handles empty queues`, () => {
-        expect(getQueueLength([])).toEqual({ totalDuration: 0, numLiveVideos: 0 });
+        expect(getQueueLength([])).toEqual({ totalDuration: 0, numLiveVideos: 0, durationTillFirstLive: 0 });
     });
 });
