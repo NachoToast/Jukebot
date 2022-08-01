b325136e9ca5e2cfa9b2cc0c298ab9085900968b
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-08-01 19:33:21 +1200

    queue time handling of live videos

--- src/classes/Jukebox/EmbedBuilders/NowPlaying.ts
+++ src/classes/Jukebox/EmbedBuilders/NowPlaying.ts
@@ -5,16 +5,12 @@ import { Config } from '../../../global/Config';
 import { viewCountFormatter } from '../../../functions/viewCountFormatter';
 import { Jukebox } from '../Jukebox';
 import { numericalToString } from '../../../functions/timeConverters';
+import { getQueueLength } from '../../../functions/getQueueLength';
 
 /** Makes a "now playing X" embed. */
 export function makeNowPlayingEmbed(jukebox: Jukebox, status: ActiveJukeboxStatus): InteractionReplyOptions {
     const disc = status.playing;
     const queueSize = jukebox.inventory.length;
-
-    let queueTime = 0;
-    for (let i = 0; i < queueSize; i++) {
-        queueTime = queueTime + jukebox.inventory[i].durationSeconds;
-    }
 
     const embed = new EmbedBuilder()
         .setTitle(disc.title)
@@ -28,7 +24,22 @@ export function makeNowPlayingEmbed(jukebox: Jukebox, status: ActiveJukeboxStatu
         .setColor(Config.embedColor);
 
     if (queueSize > 0) {
-        embed.setFooter({ text: `Queue Length: ${queueSize} | Queue Time: ${numericalToString(queueTime)}` });
+        const { totalDuration, numLiveVideos } = getQueueLength(jukebox.inventory);
+        let queueTimeString;
+        const queueTime = numericalToString(totalDuration);
+        if (numLiveVideos == queueSize) {
+            // only live videos
+            queueTimeString = `Queue Time: n/a (All live videos)`;
+        } else if (numLiveVideos === 0) {
+            // no live videos
+            queueTimeString = `Queue Time: ${queueTime}`;
+        } else {
+            // some live, some non-live videos
+            queueTimeString = `Queue Time: ${queueTime} (Excluding ${numLiveVideos} live video${
+                numLiveVideos !== 1 ? `s` : ``
+            })`;
+        }
+        embed.setFooter({ text: `Queue Length: ${queueSize} | ${queueTimeString}` });
     }
 
     return { embeds: [embed] };
