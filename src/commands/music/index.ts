018be4b39d6e8a8ed6990cd6b1504c7648dfd61f
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-08-03 01:14:58 +1200

    Add `/remove` command

--- src/commands/music/index.ts
+++ src/commands/music/index.ts
@@ -5,8 +5,23 @@ import { NowPlaying } from './nowPlaying';
 import { Pause } from './pause';
 import { Play } from './play';
 import { Q, Queue } from './queue';
+import { Remove } from './remove';
 import { Resume, Unpause } from './resume';
 import { Shuffle } from './shuffle';
 import { Skip } from './skip';
 
-export const musicCommands = [Play, Queue, Q, NowPlaying, Skip, Shuffle, Pause, Resume, Unpause, Clear, Fix, Leave];
+export const musicCommands = [
+    Play,
+    Queue,
+    Q,
+    NowPlaying,
+    Skip,
+    Shuffle,
+    Pause,
+    Resume,
+    Unpause,
+    Clear,
+    Fix,
+    Leave,
+    Remove,
+];
