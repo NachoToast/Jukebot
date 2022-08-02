0990f9b79a53e08973587fd13f2d41d4fd5b5511
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-08-03 03:07:04 +1200

    Add `/patchnotes` command

--- src/commands/util/index.ts
+++ src/commands/util/index.ts
@@ -1,4 +1,5 @@
 import { Status } from './status';
 import { Source } from './source';
+import { Patchnotes } from './patchnotes';
 
-export const utilCommands = [Status, Source];
+export const utilCommands = [Status, Source, Patchnotes];
