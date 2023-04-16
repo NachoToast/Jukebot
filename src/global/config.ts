a3b74d9d97ce2b779773637710f2d9dbc8fcec44
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-04-17 03:56:17 +1200

    remove tests from CI

--- src/global/config.ts
+++ src/global/config.ts
@@ -1,30 +1,29 @@
-import { existsSync } from 'fs';
 import { Config } from '../types';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const rawConfig: Partial<Config> = existsSync('config.json') ? require('../../config.json') : undefined;
+const rawConfig: Partial<Config> = require('../../config.json');
 
-if (rawConfig?.discordToken === undefined) {
+if (rawConfig.discordToken === undefined) {
     throw new Error('Missing discordToken in config.json');
 }
 
 function convertTimeoutThreshold(key: keyof Config['timeoutThresholds'], fallback: number) {
-    if (rawConfig?.timeoutThresholds?.[key] !== undefined) {
-        if (rawConfig?.timeoutThresholds[key] === 0) {
+    if (rawConfig.timeoutThresholds?.[key] !== undefined) {
+        if (rawConfig.timeoutThresholds[key] === 0) {
             return Infinity;
         }
-        return rawConfig?.timeoutThresholds[key];
+        return rawConfig.timeoutThresholds[key];
     }
     return fallback;
 }
 
 export const config: Config = {
-    discordToken: rawConfig?.discordToken,
-    maxQueueSize: rawConfig?.maxQueueSize ?? 1_000,
-    previousQueueSize: rawConfig?.previousQueueSize ?? 20,
-    statusTimePeriod: rawConfig?.statusTimePeriod ?? 120,
-    levenshteinThreshold: rawConfig?.levenshteinThreshold ?? 0.1,
-    volumeModifier: rawConfig?.volumeModifier ?? 0.1,
-    embedColour: rawConfig?.embedColour ?? '#794C36',
+    discordToken: rawConfig.discordToken,
+    maxQueueSize: rawConfig.maxQueueSize ?? 1_000,
+    previousQueueSize: rawConfig.previousQueueSize ?? 20,
+    statusTimePeriod: rawConfig.statusTimePeriod ?? 120,
+    levenshteinThreshold: rawConfig.levenshteinThreshold ?? 0.1,
+    volumeModifier: rawConfig.volumeModifier ?? 0.1,
+    embedColour: rawConfig.embedColour ?? '#794C36',
     timeoutThresholds: {
         discordLogin: convertTimeoutThreshold('discordLogin', 30),
         fetchResults: convertTimeoutThreshold('fetchResults', 30),
