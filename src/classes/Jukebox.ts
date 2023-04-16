b2f66ba58ed228bff1f6ffcec5edf9c6d9e647bb
NachoToast <32235595+NachoToast@users.noreply.github.com>
2023-04-16 15:28:10 +1200

    `Jukebox` near-final implementation

--- src/classes/Jukebox.ts
+++ src/classes/Jukebox.ts
@@ -1,108 +1,313 @@
-import { AudioPlayer, VoiceConnection, VoiceConnectionStatus, joinVoiceChannel } from '@discordjs/voice';
-import { ChatInputCommandInteraction, VoiceBasedChannel } from 'discord.js';
+import {
+    AudioPlayer,
+    AudioPlayerStatus,
+    AudioResource,
+    NoSubscriberBehavior,
+    VoiceConnection,
+    VoiceConnectionStatus,
+    createAudioPlayer,
+    joinVoiceChannel,
+} from '@discordjs/voice';
+import { ChatInputCommandInteraction, TextBasedChannel, VoiceBasedChannel, channelMention } from 'discord.js';
 import { JukebotGlobals } from '../global';
-import { connectionEntersState } from '../util';
+import { timeoutMessage } from '../messages';
+import { JukeboxState } from '../types';
+import { TimeoutError, capitalize, connectionEntersState, playerEntersState, withPossiblePlural } from '../util';
 import { Hopper } from './Hopper';
+import { MusicDisc } from './MusicDisc';
 
 export class Jukebox {
-    private static readonly _guildMap: Map<string, Jukebox> = new Map();
+    public readonly upcomingQueue: Hopper = new Hopper(JukebotGlobals.config.maxQueueSize);
 
-    private readonly _origin: ChatInputCommandInteraction;
-    private readonly _previousQueue: Hopper = new Hopper(JukebotGlobals.config.previousQueueSize);
-    private readonly _upcomingQueue: Hopper = new Hopper(JukebotGlobals.config.maxQueueSize);
+    public readonly historyQueue: Hopper = new Hopper(JukebotGlobals.config.previousQueueSize);
 
-    private _targetVoiceChannel: VoiceBasedChannel;
-    private _connection?: VoiceConnection;
-    private _player?: AudioPlayer;
+    /** Text channel to send messages in. */
+    private readonly _textChannel: TextBasedChannel;
 
-    private constructor(origin: ChatInputCommandInteraction, targetVoiceChannel: VoiceBasedChannel) {
-        this._origin = origin;
-        this._targetVoiceChannel = targetVoiceChannel;
+    /** Voice channel to play audio in. */
+    private _voiceChannel: VoiceBasedChannel;
+
+    private readonly _onDestroy: (guildId: string) => void;
+
+    private _connection!: VoiceConnection;
+
+    private _player!: AudioPlayer;
+
+    private _state!: JukeboxState;
+
+    public get targetVoiceChannel(): VoiceBasedChannel {
+        return this._voiceChannel;
     }
 
-    /** Returns the target voice channel of the bot, but only if it is connected to it. */
-    public getTargetVoiceChannel(): VoiceBasedChannel | undefined {
-        if (this._connection === undefined) return;
-        return this._targetVoiceChannel;
+    public get state(): JukeboxState {
+        return this._state;
     }
 
-    /** This will destroy the current connection, but not remake it. */
-    public setTargetVoiceChannel(newTarget: VoiceBasedChannel): void {
-        this._targetVoiceChannel = newTarget;
-        this.destroyConnection();
+    public constructor(
+        channel: TextBasedChannel,
+        targetVoiceChannel: VoiceBasedChannel,
+        onDestroy: (guildId: string) => void,
+    ) {
+        this._textChannel = channel;
+        this._voiceChannel = targetVoiceChannel;
+        this._onDestroy = onDestroy;
+
+        this.createConnection();
+        this.createPlayer();
+        this.createIdleState();
     }
 
-    public async connectToTarget(origin: ChatInputCommandInteraction) {
-        const connection = joinVoiceChannel({
-            channelId: this._targetVoiceChannel.id,
-            guildId: this._targetVoiceChannel.guild.id,
-            adapterCreator: this._targetVoiceChannel.guild.voiceAdapterCreator,
+    private cleanupConnection(): void {
+        this._connection.removeAllListeners();
+        if (this._connection.state.status !== VoiceConnectionStatus.Destroyed) this._connection.destroy();
+    }
+
+    private cleanupPlayer(): void {
+        this._player.removeAllListeners();
+        if (this._player.state.status !== AudioPlayerStatus.Idle) this._player.stop();
+    }
+
+    private createIdleState(): void {
+        if (this._state?.status === 'idle') clearTimeout(this._state.timeout);
+
+        if (JukebotGlobals.devmode) console.log('[Jukebox:State]: Now idle');
+
+        this._state = {
+            status: 'idle',
+            setAt: Date.now(),
+            timeout:
+                JukebotGlobals.config.timeoutThresholds.inactivity === Infinity
+                    ? undefined
+                    : setTimeout(() => {
+                          this._textChannel.send({ content: 'Left due to inactivity' }).catch(this.logError);
+                          this.destroyInstance();
+                      }, JukebotGlobals.config.timeoutThresholds.inactivity * 1_000),
+            wasPlaying:
+                this._state?.status === 'active'
+                    ? {
+                          item: this._state.currentlyPlaying,
+                          for: Date.now() - this._state.playingSince,
+                      }
+                    : undefined,
+        };
+    }
+
+    private createActiveState(currentlyPlaying: MusicDisc): void {
+        if (this._state?.status === 'idle') clearTimeout(this._state.timeout);
+
+        if (JukebotGlobals.devmode) console.log('[Jukebox:State]: Now active');
+
+        this._state = {
+            status: 'active',
+            playingSince: Date.now(),
+            currentlyPlaying,
+        };
+
+        this._textChannel.send({ content: `Now playing: ${currentlyPlaying.url}` }).catch(this.logError);
+    }
+
+    /** Returns whether there are non-bot non-deafened users in the target voice channel. */
+    public hasAudioListeners(): boolean {
+        return this._voiceChannel.members.some(({ user, voice }) => !user.bot && !voice.selfDeaf && !voice.serverDeaf);
+    }
+
+    /** Creates a connection to the target voice channel, does not ensure the connection becomes ready. */
+    private createConnection(): void {
+        if (this._connection !== undefined) this.cleanupConnection();
+
+        this._connection = joinVoiceChannel({
+            channelId: this._voiceChannel.id,
+            guildId: this._voiceChannel.guild.id,
+            adapterCreator: this._voiceChannel.guild.voiceAdapterCreator,
             selfDeaf: true,
         });
 
-        connection.on('error', (error) => {
-            origin.followUp({ content: `Connection error: ${error.message}` }).catch(() => null);
-            this.destroyConnection();
+        this._connection.on('error', (error) => {
+            this.logError(error);
+            this.destroyInstance();
         });
 
-        connection.on(VoiceConnectionStatus.Destroyed, () => this.destroyConnection());
+        if (JukebotGlobals.devmode) {
+            this._connection.on('stateChange', (oldState, newState) => {
+                if (oldState.status === newState.status) return;
 
-        connection.on(VoiceConnectionStatus.Disconnected, async () => {
+                console.log(`[Jukebox:VoiceConnection] ${oldState.status} -> ${newState.status}`);
+            });
+        }
+
+        this._connection.on(VoiceConnectionStatus.Disconnected, async () => {
             try {
-                console.log('disconnected, waiting for re-ready');
-                await connectionEntersState(connection, VoiceConnectionStatus.Ready, this._targetVoiceChannel);
+                await connectionEntersState(this._connection, VoiceConnectionStatus.Ready);
             } catch (error) {
-                this.destroyConnection();
+                this.logError(error);
+                this.destroyInstance();
             }
         });
 
+        this._connection.on(VoiceConnectionStatus.Destroyed, () => this.destroyInstance());
+    }
+
+    /** Creates a player to the target connection, does not ensure the player becomes idle. */
+    private createPlayer(): void {
+        if (this._player !== undefined) this.cleanupPlayer();
+
+        this._player = createAudioPlayer({ behaviors: { noSubscriber: NoSubscriberBehavior.Pause } });
+
+        this._player.on('error', (error) => {
+            this.logError(error);
+            this.destroyInstance();
+        });
+
+        if (JukebotGlobals.devmode) {
+            this._player.on('stateChange', (oldState, newState) => {
+                if (oldState.status === newState.status) return;
+
+                console.log(`[Jukebox:AudioPlayer] ${oldState.status} -> ${newState.status}`);
+            });
+        }
+
+        this._player.on(AudioPlayerStatus.Idle, () => this.createIdleState());
+
+        this._connection.subscribe(this._player);
+    }
+
+    public async ensureConnectionReady(): Promise<void> {
+        return void (await connectionEntersState(this._connection, VoiceConnectionStatus.Ready));
+    }
+
+    public updateTargetVoiceChannel(newVoiceChannel: VoiceBasedChannel): void {
+        this._voiceChannel = newVoiceChannel;
+        this.createConnection();
+    }
+
+    public handleChannelDrag(newVoiceChannel: VoiceBasedChannel): void {
+        this._voiceChannel = newVoiceChannel;
+    }
+
+    /**
+     * Attempts to play the next song in the queue (if it exists).
+     *
+     * @param {ChatInputCommandInteraction} [requester] An interaction to log the result to, this interaction should
+     * already have been replied to.
+     * @returns {Promise<JukeboxState>} The new state of the Jukebox.
+     */
+    public async playNextInQueue(requester?: ChatInputCommandInteraction): Promise<JukeboxState> {
+        const logToRequesterOrChannel: (content: string) => void =
+            requester === undefined
+                ? (content) => this._textChannel.send({ content }).catch(this.logError)
+                : (content) => requester.editReply({ content }).catch(this.logError);
+
+        if (JukebotGlobals.devmode)
+            console.log(`[Jukebox] Playing next in queue (${this.upcomingQueue.getLength().totalItems})`);
+
+        const nextItem = this.upcomingQueue.getNext();
+
+        if (nextItem === undefined) {
+            this.createIdleState();
+            return this._state;
+        }
+
+        this.historyQueue.addItems([nextItem]);
+
+        let resource: AudioResource<MusicDisc>;
+
+        // connecting
+        if (this._connection.state.status !== VoiceConnectionStatus.Ready) {
+            requester
+                ?.editReply({ content: `Connecting to ${channelMention(this._voiceChannel.id)}` })
+                .catch(this.logError);
+
+            try {
+                await connectionEntersState(this._connection, VoiceConnectionStatus.Ready);
+            } catch (error) {
+                if (error instanceof TimeoutError) logToRequesterOrChannel(timeoutMessage.connect(this._voiceChannel));
+                else this.logError(error);
+
+                this.createIdleState();
+                return this._state;
+            }
+        }
+
+        // preparing
+        if (!nextItem.hasResource()) {
+            requester?.editReply({ content: 'Preparing audio...' }).catch(this.logError);
+            try {
+                resource = await nextItem.getResource();
+            } catch (error) {
+                if (error instanceof TimeoutError) logToRequesterOrChannel(timeoutMessage.generateResource(nextItem));
+                else this.logError(error);
+
+                return await this.playNextInQueue(requester);
+            }
+        } else resource = await nextItem.getResource();
+
+        // playing
+        requester?.editReply({ content: 'Playing...' }).catch(this.logError);
         try {
-            await connectionEntersState(connection, VoiceConnectionStatus.Ready, this._targetVoiceChannel);
-            this._connection = connection;
+            this._player.play(resource);
+
+            await playerEntersState(this._player, AudioPlayerStatus.Playing);
         } catch (error) {
-            this.destroyConnection();
-            throw error;
+            if (error instanceof TimeoutError) logToRequesterOrChannel(timeoutMessage.play);
+            else this.logError(error);
+
+            return await this.playNextInQueue(requester);
+        }
+
+        this.createActiveState(nextItem);
+
+        return this._state;
+    }
+
+    /** Cleans up the player and connection, and executes the `onDestroy` callback function. */
+    public destroyInstance(): void {
+        if (JukebotGlobals.devmode) console.log('[Jukebox] Instance destroyed');
+
+        this.cleanupPlayer();
+        this.cleanupConnection();
+        this._onDestroy(this._voiceChannel.guild.id);
+    }
+
+    /**
+     * (If in devmode or receiving an unknown object) Logs an error to the console.
+     *
+     * (Otherwise) Attempts to send an error message in the text channel of this Jukebox instance.
+     */
+    private async logError(error: unknown): Promise<void> {
+        if (!(error instanceof Error) || JukebotGlobals.devmode) {
+            console.log(error);
+            return;
+        }
+
+        try {
+            await this._textChannel.send({ content: `Error: ${error.message}` });
+        } catch (anotherError) {
+            // an error occurred trying to follow up the interaction
+            // this is likely due to the interaction being deleted
+            // in any case we don't want to try informing the user of the error again
+            // since it will most likely result in another error
         }
     }
 
-    public destroyConnection() {
-        if (this._connection === undefined) return;
-        this._connection.removeAllListeners();
-        this._connection.destroy();
-        delete this._connection;
-    }
+    public toString(): string {
+        const output: string[] = [
+            `Jukebox(${this._voiceChannel.guild.name})`,
+            `Connection(${channelMention(this._voiceChannel.id)}): ${capitalize(this._connection.state.status)}`,
+            `Player: ${capitalize(this._player.state.status)}`,
+        ];
 
-    public handleDragged(newVoiceChannel: VoiceBasedChannel): void {
-        this._targetVoiceChannel = newVoiceChannel;
-    }
+        if (this._state.status === 'active') {
+            const playingFor = Math.floor((Date.now() - this._state.playingSince) / 1_000);
 
-    public getHasListenersInVoice(): boolean {
-        return this._targetVoiceChannel.members.some((e) => !e.user.bot && !e.voice.selfDeaf && !e.voice.serverDeaf);
-    }
+            output.push(`State: Active (${withPossiblePlural(playingFor)} ${this._state.currentlyPlaying.url})`);
+        } else {
+            const inactiveTimeRemaining =
+                JukebotGlobals.config.timeoutThresholds.inactivity -
+                Math.floor((Date.now() - this._state.setAt) / 1_000);
 
-    public static getJukebox(guildId: string): Jukebox | undefined {
-        return Jukebox._guildMap.get(guildId);
-    }
+            output.push(`State: Inactive (${withPossiblePlural(inactiveTimeRemaining)} left)`);
+        }
 
-    public static makeJukebox(origin: ChatInputCommandInteraction, targetVoiceChannel: VoiceBasedChannel): Jukebox {
-        const jukebox = new Jukebox(origin, targetVoiceChannel);
-        Jukebox._guildMap.set(targetVoiceChannel.guild.id, jukebox);
-        return jukebox;
-    }
-
-    public static getOrMakeJukebox(
-        origin: ChatInputCommandInteraction,
-        targetVoiceChannel: VoiceBasedChannel,
-    ): Jukebox {
-        const jukebox = Jukebox.getJukebox(targetVoiceChannel.guild.id);
-        if (jukebox !== undefined) return jukebox;
-        return Jukebox.makeJukebox(origin, targetVoiceChannel);
-    }
-
-    public static destroyJukebox(guildId: string): void {
-        const jukebox = Jukebox.getJukebox(guildId);
-        if (jukebox === undefined) return;
-        // TODO: call destroy method of instance here
-        Jukebox._guildMap.delete(guildId);
+        return output.join('\n');
     }
 }
--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,5 +1,6 @@
 export * from './Allay';
 export * from './CommandDeployer';
+export * from './EntityManager';
 export * from './Hopper';
 export * from './Jukebox';
 export * from './MusicDisc';
