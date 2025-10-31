b6680bb3c3be8542cef6fc31bb4bddb79b39fb3a
Skylar <biscuitmunch0@gmail.com>
2025-10-31 20:04:28 +1300

    fix: hopefully this works (#172)

--- src/classes/Cobalt.ts
+++ src/classes/Cobalt.ts
@@ -110,7 +110,7 @@ export class Cobalt {
             body: JSON.stringify({
                 url,
                 downloadMode: 'audio',
-                audioFormat: 'wav',
+                audioFormat: 'mp3',
             }),
         });
 
